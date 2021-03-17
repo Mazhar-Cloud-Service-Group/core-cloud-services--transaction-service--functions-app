6fb3f946566cc01970277ba1045802d5076b007a
mazhar.ahmad@wavenetcorp.com <mazhar.ahmad@wavenetcorp.com>
2021-03-17 17:16:58 +0530

    Added first Solution and CSProj file

---
+++ Core.Cloud.Services.TransactionService.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Core.Cloud.Services.TransactionService.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
