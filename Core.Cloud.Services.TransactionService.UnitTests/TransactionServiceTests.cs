1acd2f752800aecaf54228a06b198403fc0eab35
mazhar.ahmad@wavenetcorp.com <mazhar.ahmad@wavenetcorp.com>
2021-09-03 18:59:03 +0530

    Add first Unit Test

---
+++ Core.Cloud.Services.TransactionService.UnitTests/TransactionServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Cloud.Services.TransactionService.UnitTests
+{
+    [TestClass]
+    public class TransactionServiceTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+
+        }
+
+        [TestMethod]
+        public void SuccesspullyCreatedTransaction()
+        {
+            var name = "TransactionService";
+            var serviceName = name;
+
+            Assert.AreEqual(serviceName, name);
+        }
+    }
+}
